a50fe53fbbf9884ada39e8ee20d7da475ddaf587
ram3ez <rameez12m@gmail.com>
2025-05-21 18:15:55 +0400

    device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import "package:device_preview/device_preview.dart";
 import "package:flutter/cupertino.dart";
+import "package:flutter/foundation.dart";
 //import "package:flutter/material.dart";
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => MyApp()));
+  runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
